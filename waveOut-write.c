998d6b25e610f25bff40055cebcf32995b36e7cb
Benjamin Cook <root@baryon.it>
2022-03-03 14:33:28 -0800

    buffered stdin!

--- waveOut-write.c
+++ waveOut-write.c
@@ -5,44 +5,68 @@
 #include <windows.h>
 #include <mmsystem.h>
 #include <stdio.h>
+#include <fcntl.h>
 
 #define BLK_N 8
 #define BLK_LEN 16384
 
 int main(){
-	char *buffer;
-	long fileSize;
-
-	fseek(stdin, 0, SEEK_END);
-	fileSize = ftell(stdin);
-	fseek(stdin, 0, SEEK_SET);
-	buffer = (char*) calloc(1, fileSize+1);
-	fread(buffer, 1, fileSize, stdin);
+	char *buffer=calloc(BLK_N,BLK_LEN);
+	char *buf[BLK_N];
+	for (int i=0;i<BLK_N;i++)
+		buf[i]=buffer+(i*BLK_LEN);
 
 	WAVEFORMATEX wf;
-	WAVEHDR wh;
-	HWAVEOUT hWaveOut;
-
 	wf.wFormatTag = WAVE_FORMAT_PCM;
-	wf.nChannels = 2;
-	wf.nSamplesPerSec = 44100;
-	wf.nAvgBytesPerSec = 44100 * 2 * 2;
-	wf.nBlockAlign = 4;
-	wf.wBitsPerSample = 16;
+	wf.nBlockAlign = ( wf.wBitsPerSample = 16 ) >> 3 * ( wf.nChannels = 2);
+	wf.nAvgBytesPerSec = ( wf.nSamplesPerSec = 44100 ) * wf.nBlockAlign;
 	wf.cbSize = 0;
 
-	wh.lpData = buffer;
-	wh.dwBufferLength = fileSize;
-	wh.dwFlags = 0;
-	wh.dwLoops = 0;
+	WAVEHDR wh[BLK_N];
+	for (int i=0;i<BLK_N;i++){
+		wh[i].lpData = buf[i];
+		wh[i].dwBufferLength = BLK_LEN;
+		wh[i].dwFlags = (wh[i].dwLoops = 0);
+	}
 
+
+	HWAVEOUT hWaveOut;
 	waveOutOpen(&hWaveOut,WAVE_MAPPER,&wf,0,0,CALLBACK_NULL);
-	waveOutPrepareHeader(hWaveOut,&wh,sizeof(wh));
-	waveOutWrite(hWaveOut,&wh,sizeof(wh));
 
-	do {} while (!(wh.dwFlags & WHDR_DONE));
+	int l;
+	_setmode(_fileno(stdin), _O_BINARY);
 
-	waveOutUnprepareHeader(hWaveOut,&wh,sizeof(wh));
+	for (int i=0;i<BLK_N;i++){
+		l=fread(buf[i],1,BLK_LEN,stdin);
+	}
+
+	for (int i=0;i<BLK_N;i++){
+		waveOutPrepareHeader(hWaveOut,&wh[i],sizeof(wh));
+	}
+
+	for (int i=0;i<BLK_N;i++){
+		waveOutWrite(hWaveOut,&wh[i],sizeof(wh));
+	}
+
+	int ef=0;
+	do {
+		ef=0;
+		for (int i=0;i<BLK_N;i++){
+			if(wh[i].dwFlags & WHDR_DONE){
+				waveOutUnprepareHeader(hWaveOut,&wh[i],sizeof(wh));
+				l=fread(buf[i],1,BLK_LEN,stdin);
+				wh[i].dwBufferLength = l;
+				ef=l<1;
+				waveOutPrepareHeader(hWaveOut,&wh[i],sizeof(wh));
+				waveOutWrite(hWaveOut,&wh[i],sizeof(wh));
+				break;
+			};
+		}
+	} while (!ef);
+
+	for (int i=0;i<BLK_N;i++)
+		waveOutUnprepareHeader(hWaveOut,&wh[i],sizeof(wh));
+
 	waveOutClose(hWaveOut);
 
 	free(buffer);
