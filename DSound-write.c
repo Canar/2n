a424ed419dc0a8d63213696734481e8bb639c896
Benjamin Cook <root@baryon.it>
2024-06-09 18:18:31 -0700

    hax

--- DSound-write.c
+++ DSound-write.c
@@ -9,12 +9,13 @@
 #include <dsound.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <io.h>
 #include <fcntl.h>
 
-#define BUFFER_SIZE 1024*256
+#define BUFF 1024
+#define BUFFER_SIZE BUFF*256
 #define BUFFER_SIZE_BYTES BUFFER_SIZE * CHAN_ * BPS / 8 
+#define ACTFRAC BUFFER_SIZE_BYTES / 5 //what proportion of the buffer is worth writing to?
 
 int trunc_align(int val,int mult){ return ( val / mult ) * mult ; }
 void check_result(HRESULT result,char* file,int line) {
@@ -34,71 +35,63 @@ void wf_init(WAVEFORMATEX* wf){
 void dsbd_init(DSBUFFERDESC* dsbd,WAVEFORMATEX* wf){
 	dsbd->dwSize = sizeof(DSBUFFERDESC);
 	dsbd->dwFlags = DSBCAPS_GLOBALFOCUS | DSBCAPS_CTRLVOLUME | DSBCAPS_GETCURRENTPOSITION2;
-	dsbd->dwBufferBytes = BUFFER_SIZE_BYTES; // Buffer size in bytes
+	dsbd->dwBufferBytes = BUFFER_SIZE_BYTES;
 	dsbd->lpwfxFormat = wf;
 }
 
 int main(int argc, char** argv) {
+	_setmode(_fileno(stdin), _O_BINARY); //required
+
 	LPDIRECTSOUND lpds;
 	HRESULT result;
 	DSCK(DirectSoundCreate(0,&lpds,0));	
-	struct IDirectSoundVtbl* dst = lpds->lpVtbl;
 	
 	HWND hwnd = GetConsoleWindow();
+	struct IDirectSoundVtbl* dst = lpds->lpVtbl;
 	DSCK(dst->SetCooperativeLevel(lpds, hwnd, DSSCL_PRIORITY));
 
-	WAVEFORMATEX wf = {0}; wf_init(&wf);
-	DSBUFFERDESC dsbdesc = {0};dsbd_init(&dsbdesc,&wf);
+	WAVEFORMATEX wf={0}; wf_init(&wf);
+	DSBUFFERDESC dsbdesc={0}; dsbd_init(&dsbdesc,&wf);
 
 	LPDIRECTSOUNDBUFFER lpdsb;
 	DSCK(dst->CreateSoundBuffer(lpds, &dsbdesc, &lpdsb, NULL));
 	 
-	DWORD playCursor, writeCursor, bufferSize, lockSize, written;
-	BYTE buffer[BUFFER_SIZE_BYTES];
-	_setmode(_fileno(stdin), _O_BINARY); //required
+	void* data;
+	DWORD size,bytes_read,written_to=0,play_cursor,new_written_to,wanted;
+	struct IDirectSoundBufferVtbl* dsbt=lpdsb->lpVtbl;
 
-	int bytes_read;
-	char* data;
-	uint32_t size,wanted,written_to=0;
-	DSCK(lpdsb->lpVtbl->Lock(lpdsb, 0, BUFFER_SIZE_BYTES, &data, &size, 0, 0, DSBLOCK_ENTIREBUFFER));
-	DSCK(lpdsb->lpVtbl->Unlock(lpdsb, data, bytes_read=fread(data, 1, size, stdin), 0, 0));
-	DSCK(lpdsb->lpVtbl->Play(lpdsb, 0, 0, DSBPLAY_LOOPING));
-
-#define ACTFRAC BUFFER_SIZE_BYTES / 5
+	DSCK(dsbt->Lock(lpdsb, 0, BUFFER_SIZE_BYTES, &data, &size, 0, 0, DSBLOCK_ENTIREBUFFER));
+	DSCK(dsbt->Unlock(lpdsb, data, bytes_read=fread(data, 1, (size_t)size, stdin),0,0));
+	DSCK(dsbt->Play(lpdsb, 0, 0, DSBPLAY_LOOPING));
 
 	while (bytes_read>0) {
-		DSCK(lpdsb->lpVtbl->GetCurrentPosition(lpdsb, &playCursor, 0));
+		DSCK(dsbt->GetCurrentPosition(lpdsb, &play_cursor, 0));
 		
-		if(written_to==0){ // if you've written to the end
-			if(playCursor < ACTFRAC){ // and it isn't part played yet, sleep
-				wanted=0;
-			}else{ // otherwise, catch up
-				wanted=trunc_align(playCursor - 1,4096);
-			}
-		}else{
-			if(playCursor < written_to){ // if the cursor is behind where is written to, try remedy that
-				wanted=BUFFER_SIZE_BYTES - written_to;
-			}else{
-				wanted=trunc_align(playCursor - written_to - 1,1024); // otherwise, just try keep up
-			}
-		}
+		wanted= written_to==0 ? ( (                      //if you've written to the end
+			play_cursor < ACTFRAC ) ? (                   //and it isn't part played yet
+				0 ) : (                                    //signal sleep,
+				trunc_align(play_cursor-1,BUFF) )          //otherwise, catch up,
+			) : ( play_cursor < written_to ? (            //but if you can write to the end,
+				BUFFER_SIZE_BYTES - written_to ) : (       //do so,
+				trunc_align(play_cursor-written_to-1,BUFF) //otherwise, write to the cursor
+		) ) ;
 
-		if(( wanted < ACTFRAC) && (!(wanted + written_to == BUFFER_SIZE_BYTES))){ //ignore small write unless it fills buffer
-			Sleep(100); 
+		if(( wanted < ACTFRAC )&&(!( wanted+written_to == BUFFER_SIZE_BYTES ))){ //ignore small write unless it fills buffer
+			Sleep(1000 / 24); //cinematic!
 		}else{
-			DSCK(lpdsb->lpVtbl->Lock(lpdsb, written_to, wanted, &data, &size, 0, 0, 0 ));
-			DSCK(lpdsb->lpVtbl->Unlock(lpdsb, data, bytes_read = fread(data, 1, size, stdin), 0, 0));
-			int new_written_to=written_to+bytes_read;
+			DSCK(dsbt->Lock(lpdsb,written_to,wanted,&data,&size,0,0,0));
+			DSCK(dsbt->Unlock(lpdsb,data,bytes_read = fread(data,1,size,stdin),0,0));
+			new_written_to=written_to+bytes_read;
 			written_to = new_written_to==BUFFER_SIZE_BYTES ? 0 : new_written_to;
 			if(bytes_read==0){ //EOF -> write a little silence at the end so imprecisely timed stop doesn't matter
-				DSCK(lpdsb->lpVtbl->Lock(lpdsb, written_to, 1024, &data, &size, 0, 0, 0 ));
-				memset(data,size,0);
-				DSCK(lpdsb->lpVtbl->Unlock(lpdsb, data, size, 0, 0));
+				DSCK(dsbt->Lock(lpdsb,written_to,wanted,&data,&size,0,0,0));
+				memset(data,0,size);
+				DSCK(dsbt->Unlock(lpdsb,data,size,0,0));
 			}
 		}
 	}
-	Sleep(((BUFFER_SIZE_BYTES-playCursor)+written_to) * 10 / 441 / 4);
-	DSCK(lpdsb->lpVtbl->Stop(lpdsb));
+	Sleep(((BUFFER_SIZE_BYTES-play_cursor)+new_written_to) * 10 / 441 / 4);
+	DSCK(dsbt->Stop(lpdsb));
 	return 0;
 }
 
--- Makefile
+++ Makefile
@@ -45,7 +45,7 @@ ds-test: tmp.raw DSound-write.exe
 	cat tmp.raw | wine DSound-write.exe
 
 tmp.raw:
-	ffmpeg -loglevel -8 -i /media/gondolin/audio/seedbox/2303/*Tunes\ 2*/$(shell printf '%02d' $$(( $$RANDOM % 17 + 1 )))*.flac -ac 2 -ar 44100 -f s16le -t 8 tmp.raw
+	ffmpeg -loglevel -8 -i /media/gondolin/audio/seedbox/2303/*Tunes\ 2*/$(shell printf '%02d' $$(( $$RANDOM % 17 + 1 )))*.flac -ac 2 -ar 44100 -f s16le -ss 5 -t 30 tmp.raw
 
 ds-backup: DSound-write.c.bak
 
@@ -54,5 +54,30 @@ DSound-write.c.bak:
 
 clean-ds:
 	rm DSound-write.exe tmp.raw
+
+w: w.exe
+
+w.exe: wasapi.c
+	x86_64-w64-mingw32-gcc  wasapi.c -lole32 -luuid -lksuser -lavrt -o w.exe
+
+w-test: w.exe tmp.raw
+	cat tmp.raw | wine w.exe
+
+x: x.exe
+
+x.exe:
+	$(MINGW_CPP) -o x x.c -lole32 -lxaudio2_8
+
+x-test: x.exe tmp.raw
+	cat tmp.raw | wine x.exe
+
+k: k.exe
+
+k.exe: k.c
+	$(MINGW_CPP) -o k k.c -lsetupapi -lksuser
+
+k-test: k.exe tmp.raw
+	cat tmp.raw | wine k.exe
+
 
 .PHONY: install debug tcc waveout DSound-write.c.bak
